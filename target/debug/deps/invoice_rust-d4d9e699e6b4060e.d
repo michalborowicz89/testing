b963524eff81287c0867d58ef66599e158fc3b4e
michal.borowicz@10clouds.com <michal.borowicz@10clouds.com>
2022-12-29 09:42:08 +0100

    rust test

---
+++ target/debug/deps/invoice_rust-d4d9e699e6b4060e.d
@@ -0,0 +1,5 @@
+/Users/michalborowicz/projects/priv/invoice_rust/target/debug/deps/invoice_rust-d4d9e699e6b4060e: src/main.rs
+
+/Users/michalborowicz/projects/priv/invoice_rust/target/debug/deps/invoice_rust-d4d9e699e6b4060e.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/invoice_rust.d
@@ -0,0 +1,1 @@
+/Users/michalborowicz/projects/priv/invoice_rust/target/debug/invoice_rust: /Users/michalborowicz/projects/priv/invoice_rust/src/main.rs
